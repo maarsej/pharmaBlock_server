e81d68bdc23c21c426a169f1d9b3bb795f9b0ada
Jacob Maarse <jacob.maarse@gmail.com>
2018-05-09 15:38:56 -0400

    Minor route/server changes

--- routes/routes.js
+++ routes/routes.js
@@ -1,20 +1,23 @@
 const express = require('express');
+const cors = require('cors')
 const router = express.Router();
 const bcrypt = require('bcrypt');
 
+router.all('*', cors());
+
 module.exports = (knex) => {
 
-  router.post('/patient/login', (req, res) => {
+  router.post('/login', (req, res) => {
     console.log('got to login', req.body);
     knex.select().from('patients').where('email_address', req.body.username)
-    .then((qres) => {
-      if (bcrypt.compareSync(req.body.password, qres.password)) {
-        req.session.user_id = qres.public_address;
-        res.status(200).send(`<h1>User ${req.session.user_id} is logged in.</h1>`);
-      } else {
-        res.status(400).send('<h1>Login failed.</h1>');
-      }
-    });
+      .then((qres) => {
+        if (bcrypt.compareSync(req.body.password, qres.password)) {
+          req.session.user_id = qres.public_address;
+          res.status(200).send(`<h1>User ${req.session.user_id} is logged in.</h1>`);
+        } else {
+          res.status(400).send('<h1>Login failed.</h1>');
+        }
+      });
   });
 
   // logout request: delete cookie
@@ -31,9 +34,9 @@ module.exports = (knex) => {
     knex('contracts')
       .join('drugs', 'drugs.id', 'contracts.drug_id')
       .leftJoin('pharmacos', 'contracts.pharmaco_pubaddr', 'pharmacos.public_address')
-    .select('contracts.public_address', 'pharmacos.company_name', 'drugs.brand_name')
+      .select('contracts.public_address', 'pharmacos.company_name', 'drugs.brand_name')
       .where('patient_pubaddr', req.params.public_address)
-    .then(qres => res.json(qres));
+      .then(qres => res.json(qres));
   });
 
   // patient info
@@ -41,34 +44,34 @@ module.exports = (knex) => {
     knex.select('public_address', 'email', 'name', 'address', 'city', 'postal_code')
       .from('patients')
       .where('public_address', req.params.public_address)
-    .then((qres) => {console.log ('qres', qres); res.json(qres)})
-    .catch((err) => console.log(err));
+      .then((qres) => { console.log('qres', qres); res.json(qres) })
+      .catch((err) => console.log(err));
   });
 
   // basic pharmaceutical company contract info
   router.get('/pharmaco/:public_address/contracts', (req, res) => {
     knex('contracts')
-    .join('drugs', 'drugs.id', 'contracts.drug_id')
-    .leftJoin('pharmacos', 'contracts.pharmaco_pubaddr', 'pharmacos.public_address')
-    .select('contracts.public_address', 'pharmacos.company_name', 'drugs.brand_name')
+      .join('drugs', 'drugs.id', 'contracts.drug_id')
+      .leftJoin('pharmacos', 'contracts.pharmaco_pubaddr', 'pharmacos.public_address')
+      .select('contracts.public_address', 'pharmacos.company_name', 'drugs.brand_name')
       .where('contracts.pharmaco_pubaddr', req.params.public_address)
-    .then(qres => res.json(qres));
+      .then(qres => res.json(qres));
   });
 
-   // pharmaceutical company info
+  // pharmaceutical company info
   router.get('/pharmaco/:public_address', (req, res) => {
     knex.select('company_name', 'contact_name', 'email', 'address', 'city', 'postal_code')
       .from('pharmacos')
       .where('public_address', req.params.public_address)
-    .then(qres => res.json(qres));
+      .then(qres => res.json(qres));
   });
 
-   // pharmaceutical company info
+  // pharmaceutical company info
   router.get('/contract/:id', (req, res) => {
     knex.select()
       .from('contracts')
       .where('public_address', req.params.id)
-    .then(qres => res.json(qres));
+      .then(qres => res.json(qres));
   });
 
 
