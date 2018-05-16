ff3a0631bf7126c017e5182d32a028bcb3ae3c9b
saunderst <thomas130@gmx.com>
2018-05-16 16:52:50 -0400

    changed seeds

--- db/seeds/patients.js
+++ db/seeds/patients.js
@@ -7,33 +7,33 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('patients').insert({
           public_address: '0xBb16559B164e4f0B872caAA640Dc1CCbf1f3E8b2',
-          email: '1234@gmail.com',
+          email: 'mareli@gmail.com',
           username: 'Mareli Brooks',
           password: bcrypt.hashSync('drugs99', 10)
         }),
         knex('patients').insert({
           public_address: '0xa273e1C1Bd3FBC09b5274B2a2319193cd7298873',
-          email: '1235@gmail.com',
+          email: 'clayton@gmail.com',
           username: 'Clayton Chavez',
-          password: bcrypt.hashSync('drugs991', 10)
+          password: bcrypt.hashSync('drugs99', 10)
         }),
         knex('patients').insert({
           public_address: '0x6268c4b8490beA0880c6CC7fFFB8362c2D3B5Ff4',
-          email: '1236@gmail.com',
+          email: 'trevin@gmail.com',
           username: 'Trevin Zavala',
-          password: bcrypt.hashSync('drugs992', 10)
+          password: bcrypt.hashSync('drugs99', 10)
         }),
         knex('patients').insert({
           public_address: '0xe511708661CfAE7ae3C8Ae69810369cBA37BcE6b',
-          email: '1237@gmail.com',
+          email: 'taylor@gmail.com',
           username: 'Taylor Shea',
-          password: bcrypt.hashSync('drugs993', 10)
+          password: bcrypt.hashSync('drugs99', 10)
         }),
         knex('patients').insert({
           public_address: '0xB3AA28edf71914e0d92f56B27d5340446Bbd0b59',
-          email: '1238@gmail.com',
+          email: 'asia@gmail.com',
           username: 'Asia Walters',
-          password: bcrypt.hashSync('drugs994', 10)
+          password: bcrypt.hashSync('drugs99', 10)
         }),
        ]);
     });
--- db/seeds/pharmacos.js
+++ db/seeds/pharmacos.js
@@ -10,36 +10,36 @@ exports.seed = function(knex, Promise) {
         public_address: '0x9abbFB9219b405Fb2B0C89D4f07522CF32001A8B',
         company_name: 'GRIFOLS USA, LLC',
         contact_name: "Veradis Pinnijar",
-        email: '4560@gmail.com',
-        password: bcrypt.hashSync('drugs6660', 10)
+        email: 'thomas@gmail.com',
+        password: bcrypt.hashSync('drugs99', 10)
       }),
       knex('pharmacos').insert({
         public_address: '0x04C8559326Cd7649424708F85D45f8F924CC5b74',
         company_name: 'Physicians Total Care, Inc.',
         contact_name: "Dore Antonov",
-        email: '4561@gmail.com',
-        password: bcrypt.hashSync('drugs6661', 10)
+        email: 'ptc@gmail.com',
+        password: bcrypt.hashSync('drugs99', 10)
       }),
       knex('pharmacos').insert({
         public_address: '0xD00c9661221a448c3D6B97b968363be5A330d722',
         company_name: 'AvPAK',
         contact_name: "Giuditta Rottger",
-        email: '4562@gmail.com',
-        password: bcrypt.hashSync('drugs6662', 10)
+        email: 'avpak@gmail.com',
+        password: bcrypt.hashSync('drugs99', 10)
       }),
       knex('pharmacos').insert({
         public_address: '0xDB27926A76E7f64a25fD3dEF304E5141de09Fe1f',
         company_name: 'Proficient Rx',
         contact_name: "Giuditta Rottger",
-        email: '4563@gmail.com',
-        password: bcrypt.hashSync('drugs6663', 10)
+        email: 'prx@gmail.com',
+        password: bcrypt.hashSync('drugs99', 10)
       }),
       knex('pharmacos').insert({
         public_address: '0xBC56119401f146F5eb3852C2618207349b321062',
         company_name: 'Best Choice (Valu Merchandisers Company)',
         contact_name: "Giuditta Rottger",
-        email: '4564@gmail.com',
-        password: bcrypt.hashSync('drugs6664', 10)
+        email: 'best@gmail.com',
+        password: bcrypt.hashSync('drugs99', 10)
       })
       ]);
   });
